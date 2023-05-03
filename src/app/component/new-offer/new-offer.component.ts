509b617dc4ddc23536e12636af74bb9462cfb8c3
Jose Vara <desarrollo.vca@vcaperu.com>
2023-05-02 21:55:17 -0500

    a

---
+++ src/app/component/new-offer/new-offer.component.ts
@@ -0,0 +1,24 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Offer } from 'src/app/models/offer.model';
+import { HttpDataService } from 'src/app/services/http-data.service';
+
+@Component({
+  selector: 'app-new-offer',
+  templateUrl: './new-offer.component.html',
+  styleUrls: ['./new-offer.component.scss'],
+})
+export class NewOfferComponent {
+  @ViewChild('movieForm', { static: false })
+  offerForm!: NgForm;
+
+  offerData!: Offer;
+
+  constructor(
+    private router: Router,
+    private HttpDataService: HttpDataService
+  ) {
+    this.offerData = {} as any;
+  }
+}
